3a2894ccde53cc6e1aeb4a86a7de829e9c793af9
Conner Starkel <conner@starkel.us>
2020-02-06 17:46:31 -0800

    Write service

---
+++ src/DiscordBot/Models/FapiRequest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarBot.Models
+{
+    public class FapiRequest
+    {
+        public List<String> images;
+        public Arguments args;
+    }
+
+    public class Arguments
+    {
+        public string text;
+    }
+}
---
+++ src/DiscordBot/Services/FapiService.cs
@@ -0,0 +1,68 @@
+﻿using Newtonsoft.Json;
+using StarBot.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarBot.Services
+{
+    /*
+     * Service for fAPI 
+     * https://fapi.dreadful.tech/index.html
+     */
+    public class FapiService
+    {
+        private string API_TOKEN;
+        private readonly string ENDPOINT = "https://fapi.wrmsr.io/";
+
+        public FapiService()
+        {
+            API_TOKEN = DiscordBot.Program._config["fapiapitoken"];
+        }
+
+        public string BuildUrl(string function)
+        {
+            return ENDPOINT + function;
+        }
+
+        public HttpContent BuildImageRequest(FapiRequest body)
+        {
+            return BuildRequest(body, "image/png");
+        }
+
+        public HttpContent BuildTextRequest(FapiRequest body)
+        {
+            return BuildRequest(body, "text/plain");
+        }
+
+        public HttpContent BuildRequest(FapiRequest body, string contentType)
+        {
+            string json = JsonConvert.SerializeObject(body);
+            HttpContent request = new StringContent(json);
+            request.Headers.ContentType = MediaTypeHeaderValue.Parse(contentType);
+            request.Headers.Add("Authorization", API_TOKEN);
+            return request;
+        }
+
+        public async Task<Stream> ExecuteImageRequest(string function, HttpContent body)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = httpClient.PostAsync(BuildUrl(function), body).Result;
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStreamAsync().Result;
+        }
+
+        public async Task<String> ExecuteTextRequest(string function, HttpContent body)
+        {
+            HttpClient httpClient = new HttpClient();
+            HttpResponseMessage response = httpClient.PostAsync(BuildUrl(function), body).Result;
+            response.EnsureSuccessStatusCode();
+            return response.Content.ReadAsStringAsync().Result;
+        }
+    }
+}
