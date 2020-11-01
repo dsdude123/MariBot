286b62e542832e0d30b1688361c0591f1d0c998b
Conner Starkel <csmediaseattle@gmail.com>
2020-10-31 23:24:41 -0700

    HOTFIX: Reenable blocking commands from other bots

--- src/DiscordBot/Services/CommandHandlingService.cs
+++ src/DiscordBot/Services/CommandHandlingService.cs
@@ -36,7 +36,7 @@ namespace DiscordBot.Services
         {
             // Ignore system messages and messages from bots
             if (!(rawMessage is SocketUserMessage message)) return;
-            //if (message.Source != MessageSource.User) return;
+            if (message.Source != MessageSource.User) return;
 
             int argPos = 0;
             if (!(message.HasMentionPrefix(_discord.CurrentUser, ref argPos) || message.HasStringPrefix(Program._config["prefix"] + " ",ref argPos))) return;
