739691eea42e2c97dcc688c6b87f9ff7ed77645e
Conner Starkel <csmediaseattle@gmail.com>
2023-03-11 20:27:49 -0800

    Initial core code

---
+++ MariBot-Core/Program.cs
@@ -0,0 +1,31 @@
+using Discord.WebSocket;
+using MariBot_Core.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddLogging();
+builder.Services.AddSingleton<DiscordSocketClient>();
+builder.Services.AddHostedService<DiscordBotService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ MariBot-Core/Services/DiscordBotService.cs
@@ -0,0 +1,70 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace MariBot_Core.Services
+{
+    public class DiscordBotService : IHostedService
+    {
+        private DiscordSocketClient client;
+        private readonly IConfiguration configuration;
+        private ILogger<DiscordBotService> logger;
+
+        public DiscordBotService(DiscordSocketClient client, IConfiguration configuration, ILogger<DiscordBotService> logger)
+        {
+            this.client = client;
+            this.configuration = configuration;
+            this.logger = logger;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            logger.LogInformation("Starting up Discord bot");
+            client.Log += Log;
+            var botToken = configuration.GetValue<string>("DiscordSettings:BotToken");
+            logger.LogInformation("Got token {}", botToken);
+            await client.LoginAsync(TokenType.Bot, botToken);
+            client.StartAsync();
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            try
+            {
+                client.Dispose();
+            }
+            catch
+            {
+                logger.LogWarning("Failed to dispose Discord client.");
+            }
+        }
+
+        private Task Log(LogMessage arg)
+        {
+            var message = arg.Message;
+            if (message == null && arg.Exception != null)
+            {
+                message = arg.Exception.Message;
+            }
+            switch(arg.Severity)
+            {
+                case LogSeverity.Critical:
+                    logger.LogCritical("{} - {}", arg.Source, message);
+                    break;
+                case LogSeverity.Error:
+                    logger.LogError("{} - {}", arg.Source, message);
+                    break;
+                case LogSeverity.Warning:
+                    logger.LogWarning("{} - {}", arg.Source, message);
+                    break;
+                case LogSeverity.Info:
+                    logger.LogInformation("{} - {}", arg.Source, message);
+                    break;
+                case LogSeverity.Verbose:
+                case LogSeverity.Debug:
+                    logger.LogTrace("{} - {}", arg.Source, message);
+                    break;
+            }
+            return Task.CompletedTask;
+        }
+    }
+}
